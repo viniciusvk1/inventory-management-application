4dd088ccd1f974bed498b3902475c814739b4fec
Vinicius <viniciusalmeida.vk1@gmail.com>
2023-05-24 22:08:42 -0300

    Added methods, add, edit, delete

--- src/main/java/com/github/viniciusvk1/controller/CategoryController.java
+++ src/main/java/com/github/viniciusvk1/controller/CategoryController.java
@@ -4,6 +4,7 @@ import com.github.viniciusvk1.model.Category;
 import com.github.viniciusvk1.repository.CategoryRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,7 +29,7 @@ public class CategoryController {
     // findAllByName method -> able to search for a category by name that already exists in the system.
     // mapped by: /category/{name} | <- in the url {name} you should pass the name of the category you want to find
     // Tested status: Working
-    @GetMapping("/{name}")
+    @GetMapping("/find/{name}")
     public ResponseEntity<List<Category>> findAllByName(@Valid @PathVariable String name) {
         return ResponseEntity.ok(categoryRepository.findAllByNameContainingIgnoreCase(name));
     }
@@ -42,4 +43,42 @@ public class CategoryController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    // addCategory method -> Allows the user to add a new category to the database
+    // To add using the method you must use the post method with the following json:
+    /*
+            {
+    "name": "name",
+    "description": "description"
+}
+     */
+    // mapped by: /category
+    // Tested status: Working
+    @PostMapping
+    public ResponseEntity<Category> addCategory(@Valid @RequestBody Category category) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryRepository.save(category));
+    }
+
+    // editCategory method -> Allows the user to edit an existing category by the registered id
+    // mapped by: /category
+    // Tested Status: Working
+    @PutMapping
+    public ResponseEntity<Category> editCategory(@Valid @RequestBody Category category) {
+        return categoryRepository.findById(category.getId())
+                .map(response -> ResponseEntity.ok().body(categoryRepository.save(category)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // delCategoryById method -> Allows the user to delete a category by the generated id
+    // mapped by: /category/del/{id} -> in the {id} url an id of an existing category must be inserted to be deleted
+    // Tested Status: Working
+    @DeleteMapping("/del/{id}")
+    public ResponseEntity<?> delCategoryById(@PathVariable Long id) {
+
+        return categoryRepository.findById(id)
+                .map(response -> {
+                    categoryRepository.deleteById(id);
+                    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
